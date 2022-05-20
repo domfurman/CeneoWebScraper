3e0fdd8a1295c84a94f0e10cb6fa870520622d60
s-301-16 <s-301-16@gumiforest.uek.krakow.pl>
2022-05-20 11:15:32 +0200

    2022-05-20 DF

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+@app.route('/index/<name>')
+def index():
+    text="Hello World!!!"
+    return render_template("index html)", text=name)
+
