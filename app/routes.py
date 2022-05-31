e5d04a8158b9763bee9bb2a76a514a52a3ed79a8
xFurciK <s222915@student.uek.krakow.pl>
2022-05-31 14:18:24 +0200

    DD

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,58 @@
+import imp
 from unicodedata import name
 from app import app
 from flask import render_template
+import requests
+import json
+import os
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from bs4 import BeautifulSoup
 
 @app.route('/')
 @app.route('/index')
 @app.route('/index/<name>')
 def index():
     text="Hello World!!!"
-    return render_template("index html)", text=name)
+    return render_template("index.html.jinja", text=name)
 
-@app.route('/extract')
+@app.route('/extract/<product_id>')
+def extract(product_id):
+    url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
+    all_opinions = []
+    while(url):
+        response = requests.get(url)
+
+        page = BeautifulSoup(response.text, 'html.parser')
+
+        opinions = page.select("div.js_product-review")
+        for opinion in opinions:
+    
+            single_opinion = {
+                key:get_item(opinion, *value)
+                    for key, value in selectors.items()
+            }
+            single_opinion["opinion_id"] = opinion['data-entry-id']
+            opinion_id = opinion["data-entry-id"]
+            all_opinions.append(single_opinion)
+        try:
+            url = "https://www.ceneo.pl"+get_item(page, 'a.pagination__next', 'href')
+        except TypeError:
+            url = None
+    with open(f'opinions/{product_id}.json', 'w', encoding='UTF-8') as jf:
+        json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
+    return redirect(url_for("product", product_id=product_id))
+
+@app.route('/products')
+def products(product_id):
+    products = [filename.split(".")[0] for filename in os.listdir("./opinions")]
+    return render_template("products.html.jinja", products=products)
+
+@app.route('/author')
+def author():
+    pass
+
+@app.route('/product/<product_id>')
+def extract(product_id):
+    pass
