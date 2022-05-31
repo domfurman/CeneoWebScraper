128c609d6c15bca9e2ee780534c135fa0389768f
xFurciK <s222915@student.uek.krakow.pl>
2022-05-31 14:43:16 +0200

    XD

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-# CeneoWebScraper
-=======
 # CeneoWebScraperN11
->>>>>>> aac459b611dfbcaa35759cf0268ebce050a75da7
--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,7 @@
 from flask import Flask
-<<<<<<< HEAD
-app = Flask(__name__)
-from app import routes
-=======
 
 app = Flask(__name__)
 
 from app import routes
 
->>>>>>> aac459b611dfbcaa35759cf0268ebce050a75da7
 app.run(debug=True)
