9df90f1018daf1547f57a6fdf87e5953fece738d
xFurciK <s222915@student.uek.krakow.pl>
2022-06-14 19:54:42 +0200

    2022-06-04 DF

--- app/models/opinion.py
+++ app/models/opinion.py
@@ -1,4 +1,3 @@
-from app.routes import author
 from app.utils import get_item
 from app.parameters import selectors
 class Opinion:
@@ -16,27 +15,15 @@ class Opinion:
         self.opinion_id = opinion_id
         return self
 
-    def __str__():
-        pass
+    def __str__(self):
+        return f"opinion_id: {self.opinion_id}<br>" + "<br>".join(f"{key}: {str(getattr(self, key))}" for key in selectors.keys())
 
     def __repr__(self):
-        pass
+        return f"Opinion(opinion_id={self.opinion_id}, " + ", ".join(f"{key}={str(getattr(self, key))}" for key in selectors.keys()) + ")"
 
     def to_dict(self):
-        return {
-            "author" = self.author
-            "recommendation" = self.recommendation
-            "stars" = self.stars
-            "content" = self.content
-            "useful" = self.useful
-            "useless" = self.useless
-            "publish_date" = self.publish_date
-            "purchase_date" = self.purchase_date
-            "pros" = self.pros
-            "cons" = self.cons
-            "opinion_id" = self.opinion_id
-        }
-    
+        return {"opinion_id": self.opinion_id} | {key: getattr(self, key) for key in selectors.keys()}
+
     def extract_opinion(self, opinion):
         for key, value in selectors.items():
             setattr(self, key, get_item(opinion, *value))
